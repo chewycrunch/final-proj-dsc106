9404e72489ca57454949b0d431f066dfef1c9e43
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-05-27 16:26:47 -0700

    setup layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
 	import '../app.css';
+	import '../scroll.css';
+
+	import Container from '$lib/Container.svelte';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<title>title goes here</title>
+	<meta name="description" content="View your favorite basketball content." />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
+<main>
+	<Container>
+		{@render children()}
+	</Container>
+</main>
