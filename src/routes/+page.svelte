4af0e40776bb38ed1009b87c96dbb3d6641591c7
David Chew <36521499+chewycrunch@users.noreply.github.com>
2025-06-07 18:58:48 -0700

    height in progres fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -257,7 +257,7 @@
 				class="absolute inset-0 overflow-y-auto transition-opacity duration-300"
 				style="opacity: {isTransitioning ? 0 : 1}"
 			>
-				<Container class="h-full py-10">
+				<Container class="min-h-full py-10">
 					<svelte:component this={slide.content} {cases} {...slide.props || {}} />
 					<!-- {@render slideContent(slide)} -->
 				</Container>
